d8c9a9f6e364de3b8da129aa0a9f3b5ca2b555d6
Daniel E. Doherty <ded@ddoherty.net>
2024-09-07 02:16:45 -0500

    Skeleton files made by `bundle gem`.

---
+++ lib/fat_fin.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "fat_fin/version"
+
+module FatFin
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/fat_fin_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe FatFin do
+  it "has a version number" do
+    expect(FatFin::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
