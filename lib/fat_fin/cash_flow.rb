49e651b520e8e074b61d293cf8b8622218a1d815
Daniel E. Doherty <ded@ddoherty.net>
2024-09-12 10:49:52 -0500

    Provide for verbose messages in irr method; return NAN if no convergence.

--- lib/fat_fin/cash_flow.rb
+++ lib/fat_fin/cash_flow.rb
@@ -30,22 +30,28 @@ module FatFin
       payments.sum(BigDecimal('0.0')) { |pmt| pmt.value_on_prime(on_date, rate: rate, freq: freq) }
     end
 
-    def irr(eps = 0.000000001)
     # Compute the internal rate of return (IRR) for the CashFlow using the
     # Newton-Raphson method and always assuming annual compounding.
+    def irr(eps = 0.000001, verbose: false)
       return BigDecimal('0.0') if payments.empty?
 
       first_date = payments.first&.date || Date.today
       try_irr = 0.5
       iters = 0
       while (npv = value_on(first_date, rate: try_irr, freq: 1)).abs > eps
-        break if iters > 1000
+        return Float::NAN if iters > 1000
+
         npv_prime = value_on_prime(first_date, rate: try_irr, freq: 1)
         new_irr = try_irr - npv / npv_prime
-        # puts "Guess: #{try_irr.round(5)}; NPV: #{npv.round(2)}; NPV': #{npv_prime.round(2)}"
+        printf "Iter: %d, Guess: %4.8f; NPV: %4.12f; NPV': %4.12f\n", iters, try_irr, npv, npv_prime if verbose
+        if (new_irr - try_irr).abs <= eps
+          puts "Guess not changing: we're done'" if verbose
+          break
+        end
         try_irr = new_irr
         iters += 1
       end
+      printf "Iter: %d; Guess: %4.4f; NPV: %4.12f; NPV': %4.12f\n", iters, try_irr, npv, npv_prime if verbose
       try_irr
     end
   end
