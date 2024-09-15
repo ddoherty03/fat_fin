9d1a65cf327c3111ecc7351693539653de1a76ae
Daniel E. Doherty <ded@ddoherty.net>
2024-09-15 06:00:01 -0500

    Add MIRR with #mirr method to compute modified IRR.

--- lib/fat_fin/cash_flow.rb
+++ lib/fat_fin/cash_flow.rb
@@ -78,7 +78,55 @@ module FatFin
       try_irr
     end
 
+    # Compute the Modified Internal Rate of Return (MIRR), also called the
+    # "Money-Weighted Rate of Return" (MWRR).  This is a much simpler
+    # computation than the IRR as it does not require Newton-Raphson.  The
+    # idea is to partition the CashFlow into negative (money paid out) and
+    # positive (money received) CashFlows.  This method uses one rate that
+    # represents the rate at which money can be borrowed in the parameter
+    # :borrow_rate and the rate money invested earns (parameter earn_rate:)
+    # over the course of the CashFlow.  This simple computes the (1) present
+    # value of the outlays, the negative amounts in the CashFlow, to the first
+    # date of the CashFlow at the borrow_rate: and (2) the future value of
+    # inflows, the positive amounts in the CashFlow, to the last date of the
+    # CashFlow at the earn_rate: and takes their ratio (FV/PV).  It the
+    # determines the annualized rate of return that would be represented by
+    # that outcome.  In computing the PV and FV, you can assume a compounding
+    # frequency with the parameter freq: other than the default of 1 per year.
+    # You can also have the FV, PV, and result printed to standard out by
+    # setting the verbose: parameter (default false) to true.
+    def mirr(earn_rate: 0.05, borrow_rate: 0.07, freq: 1, verbose: false)
+      pos_flows, neg_flows = pos_neg_partition
+      return Float::INFINITY if pos_flows.size.positive? && neg_flows.empty?
+      return 0.0 if pos_flows.empty?
+
+      fv = pos_flows.value_on(last_date, rate: earn_rate, freq: freq)
+      pv = -neg_flows.value_on(first_date, rate: borrow_rate, freq: freq)
+      years = last_date.month_diff(first_date) / 12.0
+      mirr = (fv / pv)**(1 / years) - 1.0
+      if verbose
+        puts "FV of Positive Flow at earn rate (#{earn_rate}): #{fv}"
+        puts "PV of Negative Flow at borrow rate (#{borrow_rate}): #{pv}"
+        puts "Years from first to last flow: #{years}"
+        puts "Modified internal rate of return: #{mirr}"
+      end
+      mirr
+    end
+
     private
+
+    def first_date
+      time_values.first.date
+    end
+
+    def last_date
+      time_values.last.date
+    end
+
+    def pos_neg_partition
+      pos, neg = time_values.filter { |pmt| !pmt.amount.zero? }.partition { |pmt| pmt.amount.positive? }
+      [CashFlow.new(pos), CashFlow.new(neg)]
+    end
 
     # Return the /derivative/ of the net present value of the CashFlow as of
     # the given date, using the given rate and compunding frequency.
@@ -89,7 +137,7 @@ module FatFin
     # IRR cannot be computed unless the CashFlow has at least one positive and
     # one negative value.  This tests for that.
     def mixed_signs?
-      pos, neg = time_values.filter { |pmt| !pmt.amount.zero? }.partition { |pmt| pmt.amount.positive? }
+      pos, neg = pos_neg_partition
       pos.size >= 1 && neg.size >= 1
     end
   end
