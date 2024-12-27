dab26df69ee68c8a77b63b043bb9030c487de5a9
Daniel E. Doherty <ded@ddoherty.net>
2024-12-27 05:09:54 -0600

    Extend Float class with #close_to? and #precision.

--- lib/core_ext/float.rb
+++ lib/core_ext/float.rb
@@ -1,11 +1,27 @@
 module FatFin
-  module FloatNearly
+  module FloatExtension
     refine Float do
-      # Compare floating point number for equality in $POINTS places
-      def nearly?(other, places = 7)
-        tX = sprintf("%0.#{places}f", self)
-        tY = sprintf("%0.#{places}f", other)
-        tX == tY
+      def fraction
+        abs - floor.abs
+      end
+
+      # Return the number of digits after decimal, or 0 for numbers greater
+      # than 1.
+      def precision
+        return 0 if abs > 1.0
+
+        Math.log(abs, 10).abs.round(0)
+      end
+
+      # Return whether self is within abs_tol of other.
+      def close_to?(other, abs_tol: 0.00000001, rel_tol: abs_tol)
+        return true if self == other.to_f
+        return false if infinite? || other.infinite?
+        return false if nan? || other.nan?
+
+        prec = abs_tol.precision
+        abs_diff = (round(prec) - other.round(prec)).abs.round(prec)
+        abs_diff <= [abs_tol, rel_tol * [abs, other.abs].max].max
       end
     end
   end
---
+++ spec/fat_fin/float_spec.rb
@@ -0,0 +1,49 @@
+module FatFin
+  RSpec.describe Float do
+    using FloatExtension
+
+    describe '#close_to?' do
+      context 'with magnitude between 0 and 1' do
+        let(:abs_tol) { 1.0E-14 }
+
+        it 'returns true for same positive numbers' do
+          expect(1000001.0E-15.close_to?(1000002.0E-15, abs_tol:)).to be_truthy
+          expect(1000001.0E-15.close_to?(1000001.0E-15, abs_tol:)).to be_truthy
+        end
+
+        it 'returns true for same negative numbers' do
+          expect(-1000001.0E-15.close_to?(-1000002.0E-15, abs_tol:)).to be_truthy
+          expect(-1000001.0E-15.close_to?(-1000001.0E-15, abs_tol:)).to be_truthy
+        end
+
+        it 'returns false for different positive numbers' do
+          expect(1000010.0E-15.close_to?(1000020.0E-15, abs_tol:)).to be_truthy
+          expect(1000020.0E-15.close_to?(1000010.0E-15, abs_tol:)).to be_truthy
+        end
+
+        it 'returns false for different negative numbers' do
+          expect(-1000010.0E-15.close_to?(-1000020.0E-15, abs_tol:)).to be_truthy
+          expect(-1000020.0E-15.close_to?(-1000010.0E-15, abs_tol:)).to be_truthy
+        end
+      end
+    end
+
+    describe '#precision' do
+      it "knows the precision of a small posititve" do
+        expect(0.0000000001.precision).to eq(10)
+      end
+
+      it "knows the precision of a small negative" do
+        expect(-0.0000000001.precision).to eq(10)
+      end
+
+      it "knows the precision of a large posititve" do
+        expect(123456789.1234.precision).to eq(0)
+      end
+
+      it "knows the precision of a large negative" do
+        expect(-123456789.1234.precision).to eq(0)
+      end
+    end
+  end
+end
