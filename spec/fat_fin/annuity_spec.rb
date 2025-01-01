a69f5a2111bd5b725f70c97986382a069d65c68d
Daniel E. Doherty <ded@ddoherty.net>
2025-01-01 06:29:54 -0600

    Add specs for Annuity.

--- spec/fat_fin/annuity_spec.rb
+++ spec/fat_fin/annuity_spec.rb
@@ -1,10 +1,15 @@
-  #   def test_annuities
-  #     an = Annuity.new(periods: 28, amount: 1.0)
+module FatFin
+  RSpec.describe Annuity do
+    let(:an) { Annuity.new(periods: 28, amount: 1.0) }
+    let(:eps) { 0.001 }
 
-  #     assert_in_delta(24.31644316, an.present_value(rate: 0.01), 0.01, 'Annuity[1]')
-  #     assert_in_delta(21.28127236, an.present_value(rate: 0.02), 0.01, 'Annuity[1]')
-  #     assert_in_delta(18.76410823, an.present_value(rate: 0.03), 0.01, 'Annuity[1]')
-  #     assert_in_delta(16.66306322, an.present_value(rate: 0.04), 0.01, 'Annuity[1]')
-  #     assert_in_delta(14.89812726, an.present_value(rate: 0.05), 0.01, 'Annuity[1]')
-  #     assert_in_delta(13.40616428, an.present_value(rate: 0.06), 0.01, 'Annuity[1]')
-  #   end
+    it '#present_value' do
+      expect(an.present_value(rate: 0.01)).to be_within(eps).of(24.31644316)
+      expect(an.present_value(rate: 0.02)).to be_within(eps).of(21.28127236)
+      expect(an.present_value(rate: 0.03)).to be_within(eps).of(18.76410823)
+      expect(an.present_value(rate: 0.04)).to be_within(eps).of(16.66306322)
+      expect(an.present_value(rate: 0.05)).to be_within(eps).of(14.89812726)
+      expect(an.present_value(rate: 0.06)).to be_within(eps).of(13.40616428)
+    end
+  end
+end
