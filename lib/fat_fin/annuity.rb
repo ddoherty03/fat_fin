fc698a926b4c3035875a985afcd63dfa2b846a76
Daniel E. Doherty <ded@ddoherty.net>
2025-03-24 05:12:53 -0500

    Fix missing end keyword.  Experiment Annuty2.

--- lib/fat_fin/annuity.rb
+++ lib/fat_fin/annuity.rb
@@ -22,4 +22,35 @@ module FatFin
       pv * amount
     end
   end
+
+  class Annuity2
+    attr_reader :periods, :present_value, :future_value, :rate, :payment
+
+    def initialize(periods: nil, present_value: nil, future_value: nil, rate: nil, payment: nil)
+      n_nils = [periods, present_value, future_value, rate, payment].select(&:nil?).size
+      case n_nils
+      when 0
+        raise ArgumentError, "At least one of the annuity parameters must be nil"
+      when 1
+        solve_for =
+          if periods.nil?
+            :periods
+          elsif present_value.nil?
+            :present_value
+          elsif future_value.nil?
+            :future_value
+          elsif rate.nil?
+            :rate
+          else
+            :payment
+          end
+      else
+        raise ArgumentError, "No more than one of the annuity parameters may be nil"
+      end
+    end
+
+    def to_s
+      "Annuity[#{@periods} periods of #{@amount}, compounding #{freq} times per year]"
+    end
+  end
 end
